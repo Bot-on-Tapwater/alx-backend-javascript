2656196aef1380c2259846a6c3fe6aeac8def67c
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-11-28 16:42:22 +0300

    Iterating through report objects

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,3 +1,32 @@
+/* eslint-disable no-plusplus */
 export default function createIteratorObject(report) {
+  let departmentIndex = 0;
+  let employeeIndex = 0;
 
+  const iterator = {
+    next() {
+      const departments = Object.values(report.allEmployees);
+
+      if (departmentIndex < departments.length) {
+        const employees = departments[departmentIndex];
+
+        if (employeeIndex < employees.length) {
+          const result = { value: employees[employeeIndex], done: false };
+          employeeIndex++;
+          return result;
+        }
+        // Move to the next department
+        departmentIndex++;
+        employeeIndex = 0;
+        return this.next(); // Recursive call to move to the next employee
+      }
+
+      return { done: true };
+    },
+    [Symbol.iterator]() {
+      return this;
+    },
+  };
+
+  return iterator;
 }
