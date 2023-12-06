d6c8be67d8d8d7f5e58afbf40f9b4c7c8106ea24
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-12-06 07:46:16 +0300

    Refactor task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,15 @@
-export default function cleanSet(set, startString) {
-  const strings = [];
-  for (const element of set) {
-    if (element.startsWith(startString) && startString !== '') {
-      strings.push(element.replace(startString, ''));
+const cleanSet = (set, startString) => {
+  const stringArr = [];
+
+  if (startString && startString.length > 0) {
+    for (const el of set) {
+      if (typeof el === 'string' && el.startsWith(startString)) {
+        stringArr.push(el.substring(startString.length));
+      }
     }
   }
-  return strings.join('-');
-}
+
+  return stringArr.join('-');
+};
+
+export default cleanSet;
