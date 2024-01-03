52be9235e73ee6b0297960d7e957b5e62969cd94
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-01-03 11:25:43 +0300

    Creating function specific to employees

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,61 @@
+interface DirectorInterface {
+    workFromHome(): string;
+    getToWork(): string;
+    workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+    workFromHome() {
+        return "Working from home";
+    }
+
+    getToWork() {
+        return "Getting a coffee break";
+    }
+
+    workDirectorTasks() {
+        return "Getting to director tasks";
+    }
+}
+
+class Teacher implements TeacherInterface {
+    workFromHome() {
+        return "Cannot work from home";
+    }
+
+    getCoffeeBreak() {
+        return "Cannot have a break";
+    }
+
+    workTeacherTasks() {
+        return "Getting to work";
+    }
+}
+
+function createEmployee(salary: string | number): Teacher | Director {
+    if (typeof salary === 'number' && salary < 500) {
+        return new Teacher();
+    } 
+    else {
+        return new Director();
+    }
+}
+
+function isDirector(employee: any): employee is Director {
+    return employee instanceof Director;
+}
+
+function executeWork(employee: any):  string{
+    if (isDirector(employee)) {
+        return employee.workDirectorTasks();
+    }
+    else{
+        return employee.workTeacherTasks();
+    }
+}
